c8ce539425feb82fba93af0dd77701be2c6403e8
pernillagerdin <37067302+pernillagerdin@users.noreply.github.com>
2019-10-29 14:38:07 +0100

    Add files via upload

---
+++ BLL/Episode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodcastApp
+{
+    public class Episode
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+        public Episode(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
