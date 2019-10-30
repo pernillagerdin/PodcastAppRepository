fcfdd53fb33a854b0f849679c2d258131f9a2f2e
AliciaGrevsten <alicia.grevsten@gmail.com>
2019-10-30 10:47:20 +0100

    NEW: XML_FileAccess class Validation class XML_Categories folder Categories.xml file

---
+++ BLL/Validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PodcasstApp.BLL
+{
+    class Validation
+    {
+        public static bool CharacterInputValidation(string input)
+        {
+            if (Regex.IsMatch(input, @"^[a-zA-Z]+$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CharacterInputLengthValidation(string input)
+        {
+            // Allows three words, no space in front of the first word, no numbers
+            if (Regex.IsMatch(input, @"(?=^[A-Za-z]+\s?[A-Za-z]+\s?[A-Za-z]+$).{3,30}"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool URLInputValidation(string url)
+        {
+            if (Regex.IsMatch(url, @"(?i)(http(s)?:\/\/)?(\w{2,25}\.)+\w{3}([a-z0-9\-?=$-_.+!*()]+)(?i)"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CategoryExistValidation(string category)
+        {
+            //  Check the file with the category-list if the category already exist in that file.
+            var categoryList = XML_FileAccess.LoadCategoryXMLFile();
+            bool categoryExist = false;
+
+            foreach (string categeoryItem in categoryList)
+            {
+                if (category.Equals(categeoryItem))
+                {
+                    categoryExist = true;
+                }
+            }
+
+            return categoryExist;
+        }
+
+        public static bool URLExistValidation(string category)
+        {
+            //  Check the file with the podcast-list if the url already exist in that file.
+            return false;
+        }
+    }
+}
---
+++ BLL/XML_FileAccess.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PodcasstApp.BLL
+{
+    class XML_FileAccess
+    {
+        public static void AddPodcastFile()
+        {
+
+        }
+        public static void AddToPodcastXMLFile(string title)
+        {
+
+        }
+
+        public static void AddToCategoryXMLFile(string title)
+        {
+            // https://stackoverflow.com/questions/9761363/adding-to-xml-file
+            XDocument doc = XDocument.Load("../../DAL/XML_Categories/Categories.xml");
+            XElement newElement = new XElement("category", new XAttribute("title", title));
+
+            doc.Root.Add(newElement);
+            doc.Save("../../DAL/XML_Categories/Categories.xml");
+
+        }
+
+        public static void RemoveFromCategoryXMLFile(string title)
+        {
+            // https://stackoverflow.com/questions/58620539/remove-an-item-from-xml-file-using-c-sharp
+
+            var xml = XDocument.Load("../../DAL/XML_Categories/Categories.xml");
+
+            xml.Root.Elements("category").Where(e => e.Attribute("title").Value == title).Remove();
+
+            xml.Save("../../DAL/XML_Categories/Categories.xml");
+        }
+
+        public static void RemoveFromPodcastXMLFile(string title)
+        {
+
+        }
+
+        public static List<string> LoadCategoryXMLFile()
+        {
+            var categoryList = new List<string>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../DAL/XML_Categories/Categories.xml");
+            foreach (XmlNode node in doc.DocumentElement)
+            {
+                string title = node.Attributes["title"].InnerText;
+                categoryList.Add(title);
+            }
+            return categoryList;
+        }
+
+        public static void LoadPodcastXMLFile()
+        {
+            // https://www.youtube.com/watch?v=ulzFqXb13dI&list=PLY1UVBlGVILWSg2OvxRiqeCMqBK5k8aU-&index=5&t=207s
+        }
+    }
+}
